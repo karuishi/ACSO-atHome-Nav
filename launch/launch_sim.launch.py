c1140c575b0178234bb3b8af98b011dfe61bd224
Vitória Matos <vitorianascimentomatos@gmail.com>
2024-09-29 12:09:57 -0300

    add files

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,37 @@
+import os 
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    package_name='mane'
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+
+    #incluir o gazebo launch file do pacote do gazebo_ros
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+                )]),
+
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'manebot'],
+                        output='screen')
+    
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
+
---
+++ launch/rsp.launch.py
@@ -0,0 +1,64 @@
+import os 
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+import xacro
+
+def generate_launch_description():
+
+    #checar se é pra usar o tempo do simulador?
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+
+    #processar o arquivo urdf
+    pkg_path = os.path.join(get_package_share_directory('mane'))
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control])
+    rviz_config_path = os.path.join(pkg_path,'rviz','urdf_config.rviz')
+
+
+    #criando o nó do robot_state_publisher
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+
+    #launch
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='use sim time if true'),
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='use ros2_control if true'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=rviz_config_path,
+            description='Absolute path to rviz config file'), 
+        node_robot_state_publisher,
+        joint_state_publisher_node,
+        rviz_node
+    ])
