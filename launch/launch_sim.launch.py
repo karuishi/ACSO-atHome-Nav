12101e2047ba24781b0a45149b41438a48732b96
pedro <pedrolucas.js18@gmail.com>
2024-10-26 18:28:02 -0300

    add ros2_control e  twist_mux (ainda não está sub de /cmd_vel)

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,7 +1,5 @@
-import os
-
+import os 
 from ament_index_python.packages import get_package_share_directory
-
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
@@ -9,56 +7,62 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
-
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='mane' #<--- CHANGE ME
+    package_name='mane'
 
+    # Include the rsp launch file 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+        )]), launch_arguments={'use_sim_time':'true', 'use_ros2_control': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
+    twist_mux = Node(
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params, {'use_sim_time': True}],
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros.package
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments={'extra_gazebo_args': '--ros-args --params-file' + gazebo_params_file}.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')   
+    spawn_entity = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+        output='screen'
+    )
     
-    # Include rviz2 below after creating the view.rviz file or something similar.
-    # rviz2 = Node(
-    #     package='rviz2',
-    #     namespace='',
-    #     executable='rviz2',
-    #     name='rviz2',
-    #     arguments=['-d', [os.path.join(pkg_dir, 'config', 'config_file.rviz')]]
-    # )
-
-    robot_localization = Node(
-        package = 'robot_localization',
-        executable = 'ekf_node',
-        name = 'ekf_filter_node',
-        output = 'screen',
-        parameters = [os.path.join(package_name, 'config/ekf.yaml'), {'use_sim_time': True}]
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
     )
 
-
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],  
+    )
+    
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
+        twist_mux,
         spawn_entity,
-        robot_localization,
-        # rviz2,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -1,93 +1,144 @@
 #!/usr/bin/python3
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-import launch
-from launch_ros.actions import LifecycleNode
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-import launch_ros.actions
-from launch import LaunchDescription
-
-
-import lifecycle_msgs.msg
+from launch_ros.parameter_descriptions import ParameterValue  # Add this import at the top
 import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
 
+    package_name = 'mane'
   
-
-    tf2_node = Node(package='tf2_ros',
-                    executable='static_transform_publisher',
-                    name='static_tf_pub_laser',
-                    arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser'],
-                    )
-    robot_localization = launch_ros.actions.Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[os.path.join(get_package_share_directory('mane'), 'config', 'ekf.yaml')],
-    )
+    # robot_localization = launch_ros.actions.Node(
+    #     package='robot_localization',
+    #     executable='ekf_node',
+    #     name='ekf_filter_node',
+    #     output='screen',
+    #     parameters=[os.path.join(get_package_share_directory('mane'), 'config', 'ekf.yaml')],
+    # )
      
-    slam_toolbox = launch_ros.actions.Node(
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen',
-        parameters=[{
-            'use_sim_time': False,
-            'base_frame': 'base_link',
-            'odom_frame': 'odom',
-            'map_frame': 'map',
-            'laser_frame': 'laser',
-        }
-        ],
+    # slam_toolbox = launch_ros.actions.Node(
+    #     package='slam_toolbox',
+    #     executable='async_slam_toolbox_node',
+    #     name='slam_toolbox',
+    #     output='screen',
+    #     parameters=[{
+    #         'use_sim_time': False,
+    #         'base_frame': 'base_link',
+    #         'odom_frame': 'odom',
+    #         'map_frame': 'map',
+    #         'laser_frame': 'laser_link',
+    #     }
+    #     ],
+    # )
+
+    # static_tf_odom = launch_ros.actions.Node(
+    #     package='tf2_ros',
+    #     executable='static_transform_publisher',
+    #     name='static_transform_publisher',
+    #     output='screen',
+    #     arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'odom', 'base_link'],
+    # )
+
+    # jsp = Node(
+    # package = 'joint_state_publisher',
+    # executable = 'joint_state_publisher',
+    # output = 'screen'
+    # )
+
+    package_name='mane' #<--- CHANGE ME
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    teleop_twist_joy = launch_ros.actions.Node(
-        package='teleop_twist_joy',
-        executable='teleop_node',
-        name='teleop_twist_joy',
-        output='screen',
-        parameters=[{
-            'use_sim_time': False,
-            'joy_config': 'xbox',
-            'axis_linear': 'left_stick_y',
-            
-        }
-        ],
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )])
+    # )
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
+    param = {'robot_description': ParameterValue(robot_description, value_type=str)}
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': param},
+                    controller_params_file]
     )
 
-    static_tf_odom = launch_ros.actions.Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_transform_publisher',
-        output='screen',
-        arguments=['0.0', '0.0', '0.0', '0', '0', '0', 'base_link', 'odom'],
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
     )
 
-    rviz2 = launch_ros.actions.Node(
-        package='rviz2', # rviz2 is a package in ros2 humble
-        executable='rviz2', # rviz2 is a executable in ros2 humble 
-        name='rviz2', # name of the node 
-        output='screen', # output of the node
-        parameters=[os.path.join(get_package_share_directory('mane'), 'rviz', 'config_file.rviz')],
-        # open with display tf and map
-
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
     )
 
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
-    
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+    # Launch them all!
     return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-        tf2_node,   
-        robot_localization,
-        static_tf_odom,
-        rviz2,
-        #slam_toolbox,
-
+        rsp,
+        # joystick,
+        twist_mux,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
     ])
